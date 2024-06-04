1cf0f235514b4e788dc957aad0298dff82081e0e
maleneivy <68667843+maleneivy@users.noreply.github.com>
2024-06-04 13:52:48 +0200

    Fixed issue with running prettier

--- src/data/personInfo.js
+++ src/data/personInfo.js
@@ -1,9 +1,9 @@
 const personInfo = {
-  name: "Malene Ivy Olsen",
-  occupation: "Junior front-end developer", 
+  name: 'Malene Ivy Olsen',
+  occupation: 'Junior front-end developer',
   contact: {
-    linkedIn: "",
-    gitHub: "",
+    linkedIn: '',
+    gitHub: '',
   },
   about: `I am from a beautiful small place at the coast of Finnmark and has been living in Oslo for over 10 years now.
 
@@ -11,11 +11,18 @@ const personInfo = {
   
   When I am not Coding, I love to camp, Fish, crochet Amigurumis, run and spend a lot of time with my dog and my fiancé.`,
   skills: {
-    programmingLanguages: ["Javascript"], 
-    frameworks: ["React", "Next.js", "Node.js"],
-    styles: ["CSS", "SASS", "Tailwind CSS", "Styled Components", "Bootstrap", "CSS Modules"],
-    other: ["Adobe XD", "Figma", "Adobe Photoshop"],
-  }
+    programmingLanguages: ['Javascript'],
+    frameworks: ['React', 'Next.js', 'Node.js'],
+    styles: [
+      'CSS',
+      'SASS',
+      'Tailwind CSS',
+      'Styled Components',
+      'Bootstrap',
+      'CSS Modules',
+    ],
+    other: ['Adobe XD', 'Figma', 'Adobe Photoshop'],
+  },
 };
 
 export default personInfo;
