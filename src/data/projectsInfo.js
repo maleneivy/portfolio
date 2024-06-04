b33ed285802e0d99ffb7d3fb2b0ae08f40c1c971
maleneivy <68667843+maleneivy@users.noreply.github.com>
2024-06-04 11:58:49 +0200

    Added data info

---
+++ src/data/projectsInfo.js
@@ -0,0 +1,28 @@
+const projectsInfo = [
+  {
+    title: "artful-bids", 
+    description: "This project was to create an auction website, where users can add items to be bid on and bid on items other users have put up for auction. The backend is from Noroff API.",
+    gitHubLink: "https://github.com/maleneivy/artfulBids",
+    liveSiteLink: "https://artful-bids-sm2.netlify.app",
+    img: "",
+    builtWith: ["HTML", "Javascript", "SASS", "Bootstrap"],
+  },
+  {
+    title: "omni-things",
+    description: "This project was to create an use Create React App to build an eCom store. The backend is from Noroff API.",
+    gitHubLink: "https://github.com/maleneivy/omni-things",
+    liveSiteLink: "https://omni-things.netlify.app/",
+    img: "",
+    builtWith: ["React", "Redux", "Styled Components"],
+  },
+  {
+    title: "holidaze",
+    description: "This project was to create an accommodation booking site, with both a customer and an admin-facing side. The backend is from Noroff API.",
+    gitHubLink: "https://github.com/maleneivy/holidaze",
+    liveSiteLink: "https://holidaze-exam24.netlify.app/",
+    img: "",
+    builtWith: ["React", "Next.js", "Tailwind CSS"],
+  }
+];
+
+export default projectsInfo;
