508a65d084d4e6cd5933d93a26762fcc08d65889
maleneivy <68667843+maleneivy@users.noreply.github.com>
2024-06-06 10:53:50 +0200

    Added Footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
 import GlowingEffect from '@/components/GlowingEffect';
+import Footer from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
         <GlowingEffect />
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
