7ab091cbe1c51648fa678f8747f879ba45a75263
maleneivy <68667843+maleneivy@users.noreply.github.com>
2024-06-09 00:10:50 +0200

    Added profile image and updated styling on the page to match img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,53 +23,64 @@ const Header = () => {
           <HamburgerMenu />
         </div>
       </div>
-      <div className="mx-8 mt-20 sm:mx-10 sm:mt-36">
-        <h1 className="gradient-text inline-block animate-gradient bg-gradient-to-r font-bold text-transparent">
-          {personInfo.name}
-        </h1>
-        <h3 className="mt-2">{personInfo.occupation}</h3>
-        <div className="mt-4 flex flex-col">
-          <div className="flex items-center">
-            <Icon name="location" className="text-xl text-blueGreen-200" />
-            <p className="ml-3">Oslo, Norway</p>
-          </div>
-          <div className="flex items-center">
-            <Icon name="graduate" className="text-xl text-blueGreen-200" />
-            <p className="ml-3">June - 2024</p>
-          </div>
-          <div className="flex items-center">
-            <Icon name="work" className="text-xl text-blueGreen-200" />
-            <p className="ml-3">Open for work/projects</p>
-          </div>
-          <div className="mt-6 flex gap-6 sm:hidden">
-            <Link
-              href={personInfo.contact.gitHub}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="icon-link">
-                <Icon
-                  name="gitHub"
-                  className="text-4xl hover:text-blueGreen-200"
-                />
-              </span>
-            </Link>
-            <Link
-              href={personInfo.contact.linkedIn}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="icon-link">
-                <Icon
-                  name="linkedin"
-                  className="text-4xl hover:text-blueGreen-200"
-                />
-              </span>
-            </Link>
+      <div className="mt-8 flex w-10/12 flex-col-reverse items-center justify-between sm:mx-10 sm:mt-20 sm:flex-row sm:items-start">
+        <div className="sm:w-1/2">
+          <h1 className="gradient-text mt-10 inline-block animate-gradient bg-gradient-to-r font-bold text-transparent sm:mt-0">
+            {personInfo.name}
+          </h1>
+          <h3 className="mt-2">{personInfo.occupation}</h3>
+          <div className="mt-4 flex flex-col">
+            <div className="flex items-center">
+              <Icon name="location" className="text-xl text-blueGreen-200" />
+              <p className="ml-3">Oslo, Norway</p>
+            </div>
+            <div className="flex items-center">
+              <Icon name="graduate" className="text-xl text-blueGreen-200" />
+              <p className="ml-3">June - 2024</p>
+            </div>
+            <div className="flex items-center">
+              <Icon name="work" className="text-xl text-blueGreen-200" />
+              <p className="ml-3">Open for work/projects</p>
+            </div>
+            <div className="mt-6 flex gap-6">
+              <Link
+                href={personInfo.contact.gitHub}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="icon-link">
+                  <Icon
+                    name="gitHub"
+                    className="text-4xl hover:text-blueGreen-200"
+                  />
+                </span>
+              </Link>
+              <Link
+                href={personInfo.contact.linkedIn}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="icon-link">
+                  <Icon
+                    name="linkedin"
+                    className="text-4xl hover:text-blueGreen-200"
+                  />
+                </span>
+              </Link>
+            </div>
           </div>
         </div>
+        <div className="mb-8 flex justify-center sm:mb-0 sm:w-1/2 sm:justify-end">
+          <Image
+            src="/profile.png"
+            width={400}
+            height={400}
+            className="w-full max-w-[300px] sm:max-w-[400px] md:max-w-[500px]"
+            alt="Profile image"
+          />
+        </div>
       </div>
-      <div className="hidden sm:flex sm:items-end sm:justify-between">
+      <div className="mt-10 hidden w-full px-8 sm:flex sm:items-end sm:justify-between sm:px-0">
         <div className="sm:flex-2 sm:flex sm:gap-6">
           <Link href="#about">
             <span className="link" onClick={(e) => smoothScroll(e, 'about')}>
@@ -87,32 +98,6 @@ const Header = () => {
               onClick={(e) => smoothScroll(e, 'school-projects')}
             >
               School projects
-            </span>
-          </Link>
-        </div>
-        <div className="mt-10 flex gap-6 sm:mx-10 sm:mr-10 sm:mt-10 sm:pr-10">
-          <Link
-            href={personInfo.contact.gitHub}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span className="icon-link">
-              <Icon
-                name="gitHub"
-                className="text-4xl hover:text-blueGreen-200"
-              />
-            </span>
-          </Link>
-          <Link
-            href={personInfo.contact.linkedIn}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span className="icon-link">
-              <Icon
-                name="linkedin"
-                className="text-4xl hover:text-blueGreen-200"
-              />
             </span>
           </Link>
         </div>
