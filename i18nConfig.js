8bec408b6eaa339ce6cea2bfbff042040b4ea797
maleneivy <68667843+maleneivy@users.noreply.github.com>
2024-09-05 10:20:17 +0200

    Changed default language and installed latest next/react

--- i18nConfig.js
+++ i18nConfig.js
@@ -1,5 +1,5 @@
 // i18nConfig.js
 module.exports = {
-  defaultLocale: 'en',
-  locales: ['en', 'no'],
+  defaultLocale: 'no',
+  locales: ['no', 'en'],
 };
--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -6,7 +6,7 @@ import TranslationsProvider from '@/components/TranslationsProvider';
 const i18nNamespaces = ['home'];
 
 export default async function Home({ params: { locale } }) {
-  const { t, resources } = await initTranslations(locale, i18nNamespaces);
+  const { resources } = await initTranslations(locale, i18nNamespaces);
   return (
     <TranslationsProvider
       namespaces={i18nNamespaces}
